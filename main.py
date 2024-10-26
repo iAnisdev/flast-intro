d7c81398c667a88a4e74e2a041cb5cef244428df
Anis <me.anis.dev@gmail.com>
2024-10-26 02:41:24 +0100

    :zap: HTML

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from flask import Flask , url_for , request
+from flask import Flask , url_for , request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<p>Hello, World!</p>"
+@app.get('/')
+@app.get('/<name>')
+def hello(name=None):
+    return render_template('index.html' , name=name)
+
 
 @app.route('/about')
 def about():
     return "<p>About me</p>"
-
-@app.route('/<name>')
-def greet(name):
-    return f"<p>Hello, {name}!</p>"
 
 @app.route('/user/<int:user_id>' , methods=['POST'])
 def user(user_id):
@@ -37,8 +35,8 @@ def projects():
 
 with app.test_request_context():
     print(url_for('hello'))
+    print(url_for('hello', name='John'))
     print(url_for('about'))
-    print(url_for('greet', name='John Doe'))
     print(url_for('user', user_id=1))
     print(url_for('show_subpath', subpath='subpath'))
     print(url_for('projects'))
