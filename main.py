2e68c992c2435ad7b75b117459bf739981f75b92
Anis <me.anis.dev@gmail.com>
2024-10-26 02:26:29 +0100

    :zap: subpath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -19,3 +20,8 @@ def greet(name):
 def user(user_id):
     print(type(user_id))
     return f"<p>User {user_id}</p>"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
