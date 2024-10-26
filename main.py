eb7c7c422b7dd506ced1c185e4ad942b0dd3a003
Anis <me.anis.dev@gmail.com>
2024-10-26 02:31:19 +0100

    :zap: url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -23,5 +23,16 @@ def user(user_id):
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
-    # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+with app.test_request_context():
+    print(url_for('hello'))
+    print(url_for('about'))
+    print(url_for('greet', name='John Doe'))
+    print(url_for('user', user_id=1))
+    print(url_for('show_subpath', subpath='subpath'))
+    print(url_for('projects'))
