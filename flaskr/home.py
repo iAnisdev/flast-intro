2f1056f409b19a750656743bd4bd9136ccc9bde8
Anis <me.anis.dev@gmail.com>
2024-10-27 19:19:17 +0000

    :card_file_box: Home page

--- flaskr/home.py
+++ flaskr/home.py
@@ -1,7 +1,11 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template , session
+from .auth import is_logged_in
 
 bp = Blueprint('home', __name__ , url_prefix='/' , template_folder='templates/home')
 
 @bp.route('/')
+@is_logged_in
 def home():
+    print('Home')
+    print(session['token'])
     return render_template('index.html')
